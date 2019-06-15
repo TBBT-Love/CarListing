dabd89c0bfec03324c9157868ffb49ae0239263d
Indu Bala <indubala7245@gmail.com>
2019-06-15 22:53:11 +0530

    Initial Page Setup

---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from 'components/common/Header';
+
+//src\components\common\Header.js
+
+const HomePage = () => {
+    return (
+        <div>Hello World Home Header
+     <Header />
+        </div>);
+};
+
+export default HomePage;
+
+
+
---
+++ src/components/common/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from 'components/assets/images/logo.png';
+
+const Header = () => {
+    return (
+        <div>
+            <img src={logo} alt="Logo" />
+            ABC
+        </div>
+    );
+};
+
+export default Header;
