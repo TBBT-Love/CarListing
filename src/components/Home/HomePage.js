1cc497e7064521716875fcfa5824df91e20362f7
Indu Bala <indu.bala@ihsmarkit.com>
2019-06-18 20:28:36 +0530

    Axios setUp

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import { textAction, submitAction } from 'actions/simpleActions';
-
+import axios from 'axios'
 
 const mapStateToProps = state => {
     return {
@@ -20,11 +20,34 @@ const mapDispatchToProps = (dispatch) => ({
 
 class HomePage extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            cars: [],
+            store: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3001/cars')
+            .then(json => console.log(json))
+            .then(json => json.data.cars.map(result => (
+                {
+                    modelName: result.modelName,
+                    stockNumber: result.stockNumber
+                })))
+            .then(newData => this.setState({ cars: newData.data.cars, store: newData.data }))
+            .catch(error => alert(error))
+    }
+
     render() {
         return (
             <div>
                 I am main content.
-           < button onClick={this.props.textAction} > Test redux action</button >
+
+                {this.state.cars.map(user => <li key={user.modelName}>{user.stockNumber}</li>)}
+
+                < button onClick={this.props.textAction} > Test redux action</button >
             </div >);
     }
 }
